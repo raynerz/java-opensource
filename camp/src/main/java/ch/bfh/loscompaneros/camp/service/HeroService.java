50188c287f693b2984c54821feced8b47cd16108
Cristiano Colangelo <colac1@bfh.ch>
2020-10-14 16:11:50 +0200

    Add given Hero and Party files

---
+++ camp/src/main/java/ch/bfh/loscompaneros/camp/service/HeroService.java
@@ -0,0 +1,8 @@
+package ch.bfh.swos.heroes.service;
+
+import ch.bfh.swos.heroes.model.Hero;
+
+public interface HeroService {
+
+    Hero createHero(String name);
+}
---
+++ camp/src/main/java/ch/bfh/loscompaneros/camp/service/PartyService.java
@@ -0,0 +1,8 @@
+package ch.bfh.swos.heroes.service;
+
+import ch.bfh.swos.heroes.model.Party;
+
+public interface PartyService {
+
+    Party createParty(String name);
+}
