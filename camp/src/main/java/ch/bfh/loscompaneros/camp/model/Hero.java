6de8d2e809fcb725db1be0a499c928b9cc067ea5
Rayner Dappen <raynerz@gmail.com>
2020-11-09 08:59:51 +0100

    migrating project to spring boot

--- camp/src/main/java/ch/bfh/loscompaneros/camp/model/Hero.java
+++ camp/src/main/java/ch/bfh/loscompaneros/camp/model/Hero.java
@@ -1,10 +1,11 @@
 package ch.bfh.loscompaneros.camp.model;
 
 import org.hibernate.annotations.GenericGenerator;
-
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Hero {
 
     @Id
