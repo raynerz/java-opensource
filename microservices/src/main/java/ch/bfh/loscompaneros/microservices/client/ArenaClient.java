7963b83174a1a0c5f646f19509bdd657e67b19dd
Rayner Dappen <raynerz@gmail.com>
2020-12-09 20:04:43 +0100

    Adding professor's solution while Chris and Lara solve the microservices workshop

--- microservices/src/main/java/ch/bfh/loscompaneros/microservices/client/ArenaClient.java
+++
@@ -1,9 +0,0 @@
-package ch.bfh.loscompaneros.microservices.client;
-
-import ch.bfh.loscompaneros.microservices.model.Party;
-
-import java.util.List;
-
-public interface ArenaClient {
-    String battle(List<Party> challenger);
-}
--- microservices/src/main/java/ch/bfh/loscompaneros/microservices/client/CampClient.java
+++
@@ -1,8 +0,0 @@
-package ch.bfh.loscompaneros.microservices.client;
-
-import ch.bfh.loscompaneros.microservices.model.Party;
-import org.springframework.hateoas.EntityModel;
-
-public interface CampClient {
-    EntityModel<Party> createParty(String name);
-}
