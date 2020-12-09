20baf77f6c3ddf1d95fa3437fa31d4c21f98f60b
Rayner Dappen <raynerz@gmail.com>
2020-12-09 10:56:37 +0100

    adding registry service

---
+++ registry/src/main/java/ch/bfh/loscompaneros/registry/RegistryApplication.java
@@ -0,0 +1,13 @@
+package ch.bfh.loscompaneros.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryApplication.class, args);
+	}
+
+}
