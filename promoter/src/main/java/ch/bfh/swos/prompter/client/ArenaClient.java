ad1a53b269c1df1d2d92b8e93d3729f2b6642d2b
Rayner Dappen <raynerz@gmail.com>
2020-12-11 11:10:39 +0100

    Adding feign client for arena service

--- promoter/src/main/java/ch/bfh/swos/prompter/client/ArenaClient.java
+++ promoter/src/main/java/ch/bfh/swos/prompter/client/ArenaClient.java
@@ -1,13 +1,20 @@
 package ch.bfh.swos.prompter.client;
 
 import ch.bfh.swos.prompter.model.Party;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-
+@FeignClient("arena-service")
 public interface ArenaClient {
 
-    String battle(List<Party> challangers);
+    @PostMapping("/battle")
+    String battle(@RequestBody List<Party> challangers);
 
 }
 
